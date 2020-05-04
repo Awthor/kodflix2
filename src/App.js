e3669623ae98489f817cc4bb020a5d62c73d5ebf
Aaron Hawthorne <aaronhawthorne12345@gmail.com>
2020-05-04 20:42:27 +0100

    Added my favourite movie cover

---
+++ src/App.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import Gladiator from './Gladiator.jpg';
+import './App.css';
+
+function Img() {
+  return <img src={Gladiator} alt="Gladiator" />;
+}
+export default Img;
