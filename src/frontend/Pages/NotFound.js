c2faaee20e4cbb4e8f7bb6696f2a422742331d72
Awthor <aaronhawthorne12345@gmail.com>
2020-07-01 17:56:44 +0100

    new computer first push

--- src/frontend/Pages/NotFound.js
+++ src/frontend/Pages/NotFound.js
@@ -3,7 +3,7 @@ import React from 'react';
 export default function NotFound() {
     return (
         <div>
-            <h1>Oppps... the page doesn't exist :( </h1>
+            <h1>Oppps... the page doesn't exist...:( :( </h1>
         </div>
     )
 }
