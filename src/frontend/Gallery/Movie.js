2a0e50c01956cf75f131cfe3519fc5ad7ff9c1e8
Awthor <aaronhawthorne12345@gmail.com>
2020-07-05 10:51:26 +0100

    checking props error

--- src/frontend/Gallery/Movie.js
+++ src/frontend/Gallery/Movie.js
@@ -3,11 +3,15 @@ import { Link } from 'react-router-dom';
 
 export default function Movie(props) {
   return (
-    <Link to={`/${props.id}`} className='image-cover-row-item'>
-      <img src={props.picture} alt={`${props.name}`} />
-        <div className='overlay'>
+    <Link to={"/" + props.id} className="image-cover-row-item">
+      <img 
+      src={props.picture} 
+      alt={`${props.name}`} 
+      />
+        <div className="overlay">
         <h1>{props.name}!</h1>
       </div>
     </Link>
   );
 }
+
